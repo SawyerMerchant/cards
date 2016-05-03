81542189c7bae3a0141c6722ad7f7094ceff2456
Sawyer Merchant <sawyermerchant@gmail.com>
2016-05-03 13:12:10 +0000

    Change fasthopper to localhost for heroku

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,3 +1,3 @@
 if Rails.env.production?
-    Byebug.start_server 'http://fasthopper.com', ENV.fetch("BYEBUG_SERVER_PORT", 1048).to_i
+    Byebug.start_server 'localhost', ENV.fetch("BYEBUG_SERVER_PORT", 1048).to_i
 end
